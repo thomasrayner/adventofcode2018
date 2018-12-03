2760e78364331874a5d9a99a1650d950e74db7b9
Thomas Rayner <thmsrynr@outlook.com>
2018-12-03 14:20:53 -0800

    Added Day 1 C# solution

---
+++ day1/day1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Frequency = 0;
+            string FilePath;
+            string Line;
+
+            Console.WriteLine("Enter the file path");
+            FilePath = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(FilePath))
+            {
+                Console.WriteLine("Error getting file");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            StreamReader File = new StreamReader(FilePath);
+            while ((Line = File.ReadLine()) != null)
+            {
+                int ToAdd = Convert.ToInt32(Line);
+                Frequency += ToAdd;
+            }
+
+            Console.WriteLine($"Final frequency: {Frequency}");
+            Console.ReadKey();
+        }
+    }
+}
