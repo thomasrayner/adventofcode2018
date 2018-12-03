fef8023b0f6665596c382e5366c333b4ce59e59f
Thomas Rayner <thmsrynr@outlook.com>
2018-12-03 15:25:27 -0800

    Added second part of day 1

--- day1/day1/Program.cs
+++ day1/day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace day1
@@ -8,6 +9,8 @@ namespace day1
         static void Main(string[] args)
         {
             int Frequency = 0;
+            var FrequencyHistory = new HashSet<int>();
+            bool FoundRepeat = false;
             string FilePath;
             string Line;
 
@@ -21,13 +24,21 @@ namespace day1
                 Environment.Exit(1);
             }
 
-            StreamReader File = new StreamReader(FilePath);
-            while ((Line = File.ReadLine()) != null)
+            while (!FoundRepeat)
             {
-                int ToAdd = Convert.ToInt32(Line);
-                Frequency += ToAdd;
+                StreamReader File = new StreamReader(FilePath);
+                while ((Line = File.ReadLine()) != null)
+                {
+                    if (!FrequencyHistory.Add(Frequency) && !FoundRepeat)
+                    {
+                        Console.WriteLine($"Already found {Frequency}");
+                        FoundRepeat = true;
+                    }
+                    int ToAdd = Convert.ToInt32(Line);
+                    Frequency += ToAdd;
+                }
             }
-
+            
             Console.WriteLine($"Final frequency: {Frequency}");
             Console.ReadKey();
         }
