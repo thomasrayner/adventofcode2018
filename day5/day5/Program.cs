48ae6df7a5e7f4e8a7e65282cc99eb38e7b88f15
Thomas Rayner <thmsrynr@outlook.com>
2018-12-05 09:09:16 -0800

    Day 5 pt 1 - this might be the worst peforming code I've ever written

---
+++ day5/day5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file path");
+            //string FilePath = Console.ReadLine();
+            string FilePath = @"C:\Users\thrayner\Desktop\day5-input.txt";
+            string[] Lines = File.ReadAllLines(FilePath);
+
+            if (Lines.Count() > 1)
+            {
+                Console.WriteLine("Something went wrong with your input");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            foreach (string Line in Lines)
+            {
+                string Reduced = ReduceString(Line);
+                int Before = 0;
+                int After = 1;
+                while (Before != After)
+                {
+                    Before = Reduced.Length;
+                    Reduced = ReduceString(Reduced);
+                    After = Reduced.Length;
+                }
+                Console.WriteLine($"Reduced String Length: {Reduced.Length}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string ReduceString(string Reduced)
+        {
+            int PositionAhead = 1;
+            foreach (char c in Reduced)
+            {
+                if (PositionAhead < Reduced.Length)
+                {
+                    if (char.IsLower(c) && (char.ToUpper(c) == Reduced[PositionAhead]))
+                    {
+                        Reduced = Reduced.Remove(PositionAhead - 1, 2);
+                        break;
+                    }
+                    else if (char.IsUpper(c) && (char.ToLower(c) == Reduced[PositionAhead]))
+                    {
+                        Reduced = Reduced.Remove(PositionAhead - 1, 2);
+                        break;
+                    }
+                    PositionAhead++;
+                }
+            }
+            return Reduced;
+        }
+    }
+}
